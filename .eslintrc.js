5e0036f0194940b691bea395ddb0f418cf8b1bef
Dan Kim <itsdanya@gmail.com>
2017-06-17 22:29:42 +0300

    Install ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'no-use-before-define': 'off',
+    'react/sort-comp': 'off',
+    'react/prefer-stateless-function': 'off',
+  },
+};
