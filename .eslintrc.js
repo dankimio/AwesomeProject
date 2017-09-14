8f43e46ad273aa7a6e2f0b9a1b932806aec5779c
Dan Kim <itsdanya@gmail.com>
2017-09-14 22:13:20 +0200

    Init ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,3 @@
 module.exports = {
-  extends: 'airbnb',
-  plugins: ['react', 'jsx-a11y', 'import'],
-  rules: {
-    'no-use-before-define': 'off',
-    'react/sort-comp': 'off',
-    'react/jsx-no-bind': 'off',
-    'react/forbid-prop-types': 'off',
-    'react/require-default-props': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/prefer-stateless-function': 'off',
-  },
+    "extends": "airbnb"
 };
